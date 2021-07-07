68fb33556e6a80ac7b746b5560a66dca1e0ef73f
Roman <roman.oborin@orioninc.com>
2021-07-07 16:50:29 +0300

    FizzBuzz

---
+++ src/ex/training/multithreading/FizzBuzz.java
@@ -0,0 +1,64 @@
+package ex.training.multithreading;
+
+import java.util.function.IntConsumer;
+
+public class FizzBuzz {
+    private int n;
+    private int curr = 1;
+
+    public FizzBuzz(int n) {
+        this.n = n;
+    }
+
+    // printFizz.run() outputs "fizz".
+    public synchronized void fizz(Runnable printFizz) throws InterruptedException {
+        while (curr <= n) {
+            if (curr % 3 == 0 && curr % 5 != 0) {
+                printFizz.run();
+                curr++;
+                notifyAll();
+            } else {
+                wait();
+            }
+        }
+    }
+
+    // printBuzz.run() outputs "buzz".
+    public synchronized void buzz(Runnable printBuzz) throws InterruptedException {
+        while (curr <= n) {
+            if (curr % 3 != 0 && curr % 5 == 0) {
+                printBuzz.run();
+                curr++;
+                notifyAll();
+            } else {
+                wait();
+            }
+        }
+    }
+
+    // printFizzBuzz.run() outputs "fizzbuzz".
+    public synchronized void fizzbuzz(Runnable printFizzBuzz) throws InterruptedException {
+        while (curr <= n) {
+            if (curr % 15 == 0) {
+                printFizzBuzz.run();
+                curr++;
+                notifyAll();
+            } else {
+                wait();
+            }
+        }
+    }
+
+    // printNumber.accept(x) outputs "x", where x is an integer.
+    public synchronized void number(IntConsumer printNumber) throws InterruptedException {
+        while (curr <= n) {
+            if (curr % 3 != 0 && curr % 5 != 0) {
+                printNumber.accept(curr);
+                curr++;
+                notifyAll();
+            } else {
+                wait();
+            }
+        }
+    }
+}
